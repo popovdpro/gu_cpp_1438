0ea5df69014856e8fdc06bdcbb078bd56164d10f
popovdpro <i@popovd.pro>
2021-05-16 12:47:19 +0300

    Homework for Lesson #1

---
+++ LESSON_1/src/lesson_a.cpp
@@ -0,0 +1,27 @@
+// decimal
+// binary
+// hex
+// oct
+// числовой литерал это префикс перед числом, и можно использовать _ в любом количестве раз)
+
+// x = 1234;
+// a = 0b10101;
+// y = 0x1234;
+// z = 01234;
+
+// Трансляция это:
+// препроцессинг
+// компиляция
+// ассемблирование
+// линковка
+
+// Макросборщик CMake (GNU Automake, Ninja)
+
+#include <iostream>
+
+int main() { //1TBS
+	printf("Hello World!\n");
+	std::cout << "Hello C++ world" << std::endl;
+	std::cout << __cplusplus << std::endl;
+	return 0;
+}
